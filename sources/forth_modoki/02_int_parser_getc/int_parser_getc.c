abd26047971ee40eed6557199d7cf0f950961d77
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-02 21:06:15 +0900

    02

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -4,18 +4,19 @@
 /*
 cc cl_getc.c int_parser_getc.c
 */
+#define skip_white_space(c) while(c == ' ') c = cl_getc()
+int parse_int(int c);
 
 int main() {
     int answer1 = 0;
     int answer2 = 0;
-
-    // write something here.
-
-    // sample for cl_getc() usage.
+    int* answers[] = {&answer1, &answer2};
     int c;
-
-    while((c = cl_getc()) != EOF) {
-        printf("%c\n",c );
+    int i = 0;
+    const int n = sizeof(answers)/ sizeof(int*);
+    while(i < n && (c = cl_getc()) != EOF) {
+        skip_white_space(c);
+        *answers[i++] = parse_int(c);
     }
 
     // verity result.
@@ -25,4 +26,13 @@ int main() {
     return 1;
 
 
+}
+
+int parse_int(int c) {
+    int v = 0;
+    while('0' <= c && c <= '9') {
+        v = v * 10 + c - '0';
+        c = cl_getc();
+    }
+    return v;
 }
