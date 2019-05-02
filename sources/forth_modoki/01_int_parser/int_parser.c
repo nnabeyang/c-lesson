3e031b9e86a758cac2da6287a8564e520f0b92f0
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-02 20:11:38 +0900

    01

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -3,19 +3,35 @@
 
 static const char* const input = "123 456  1203";
 
+int parse_int(const char** p);
 
 int main() {
     int answer1 = 0;
     int answer2 = 0;
     int answer3 = 0;
 
-    // write something here.
-
-
+    const char* p = input;
+    answer1 = parse_int(&p);
+    p++;
+    answer2 = parse_int(&p);
+    p++;
+    parse_int(&p);
+    p++;
+    answer3 = parse_int(&p);
     // verity result.
     assert(answer1 == 123);
     assert(answer2 == 456);
     assert(answer3 == 1203);
 
     return 1;
+}
+
+int parse_int(const char** p) {
+    int v = 0;
+    const char* c = *p;
+    while('0' <= *c && *c <= '9') {
+        v = v * 10 + (*c++ - '0');
+    }
+    *p = c;
+    return v;
 }
