a601db9f1da8802c0442a0e687895fe867b69553
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-02 22:00:35 +0900

    Add parse_one and remove parse_int

--- sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
+++ sources/forth_modoki/02_int_parser_getc/int_parser_getc.c
@@ -4,35 +4,61 @@
 /*
 cc cl_getc.c int_parser_getc.c
 */
+enum {
+    NUMBER,
+    SPACE,
+    UNKNOWN
+};
 #define skip_white_space(c) while(c == ' ') c = cl_getc()
-int parse_int(int c);
+int parse_one(int c, int* out_val, int* out_type);
 
 int main() {
     int answer1 = 0;
     int answer2 = 0;
     int* answers[] = {&answer1, &answer2};
-    int c;
     int i = 0;
     const int n = sizeof(answers)/ sizeof(int*);
-    while(i < n && (c = cl_getc()) != EOF) {
-        skip_white_space(c);
-        *answers[i++] = parse_int(c);
+    int out_val;
+    int out_type;
+    int c = cl_getc();
+    while(i < n && c != EOF) {
+        c = parse_one(c, &out_val, &out_type);
+        switch(out_type) {
+        case SPACE:
+            break;
+        case NUMBER:
+            *answers[i++] = out_val;
+            break;
+        case UNKNOWN:
+            fprintf(stderr, "' ' or [0-9] is expected, but got '%c'\n", c);
+            return 1;
+        }
     }
 
     // verity result.
     assert(answer1 == 123);
     assert(answer2 == 456);
 
-    return 1;
+    return 0;
 
 
 }
-
-int parse_int(int c) {
+int parse_one(int c, int* out_val, int* out_type) {
+  if('0' <= c && c <= '9') {
     int v = 0;
-    while('0' <= c && c <= '9') {
+    do {
         v = v * 10 + c - '0';
         c = cl_getc();
-    }
-    return v;
+    }while('0' <= c && c <= '9');
+    *out_type = NUMBER;
+    *out_val = v;
+    return c;
+  }
+  if(c == ' ') {
+        skip_white_space(c);
+        *out_type = SPACE;
+        return c;
+  }
+  *out_type = UNKNOWN;
+  return c;
 }
