3f6b169dbc1f250594dfbb1e6f703d10c70f9cfd
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-04 21:47:17 +0900

    Implement stack_pop stack contains one token

--- sources/forth_modoki/04_stack/stack.c
+++ sources/forth_modoki/04_stack/stack.c
@@ -3,9 +3,28 @@
 #include <assert.h>
 #include <stdlib.h>
 
+void stack_push(struct Stack* stack, struct Token* token) {
+    stack->tokens[stack->n++] = token;
+}
+
 struct Token* stack_pop(struct Stack* stack) {
     if(stack->n == 0) return 0;
     return stack->tokens[stack->n-- - 1];
+}
+
+static void test_pop_stack_contains_one_token() {
+    struct Stack* stack = malloc(sizeof(struct Stack) + sizeof(struct Token*) * STACK_SIZE);
+    stack->n = 0;
+    int expect_val = 123;
+    int expect_type = NUMBER;
+    struct Token input = {NUMBER, {expect_val}};
+
+    stack_push(stack, &input);
+    struct Token* actual = stack_pop(stack);
+    assert(actual->ltype == expect_type);
+    assert(actual->u.number = expect_val);
+
+    assert(0 == stack_pop(stack));
 }
 
 static void test_pop_empty() {
@@ -16,6 +35,7 @@ static void test_pop_empty() {
 
 static void unit_tests() {
     test_pop_empty();
+    test_pop_stack_contains_one_token();
 }
 
 int main() {
