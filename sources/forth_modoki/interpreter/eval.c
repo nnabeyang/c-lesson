3be2f379fae1e0e23cc3be3d69a988bc25a8cc49
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-05 23:38:06 +0900

    [WIP] Implement eval literal name case

--- sources/forth_modoki/interpreter/eval.c
+++ sources/forth_modoki/interpreter/eval.c
@@ -29,7 +29,12 @@ void eval() {
                         struct Token sum = {NUMBER, {0}};
                         sum.u.number = left->u.number + right->u.number;
                         stack_push(stack, &sum);
+                    } else {
+                        stack_push(stack, &token);
                     }
+                    break;
+                case LITERAL_NAME:
+                    stack_push(stack, &token);
                     break;
                 default:
                     printf("Unknown type %d\n", token.ltype);
@@ -37,6 +42,42 @@ void eval() {
             }
         }
     }while(ch != EOF);
+}
+
+static void assert_token(struct Token* actual, struct Token* expect) {
+    assert(actual->ltype == expect->ltype);
+    switch(actual->ltype) {
+        case NUMBER:
+            assert(actual->u.number == expect->u.number);
+            break;
+        case EXECUTABLE_NAME:
+        case LITERAL_NAME:
+            assert(strcmp(actual->u.name, expect->u.name) == 0);
+            break;
+        case OPEN_CURLY:
+        case CLOSE_CURLY:
+        case END_OF_FILE:
+            assert(actual->u.onechar == expect->u.onechar);
+            break;
+        default:
+            break;
+    }
+}
+
+static void test_eval_stack_literal_name() {
+    char *input = "/hoge 123 def";
+    struct Token expects[] = {
+        {EXECUTABLE_NAME, {.name = "def"}},
+        {NUMBER, {.number = 123}},
+        {LITERAL_NAME, {.name = "hoge"}}
+    };
+
+    cl_getc_set_src(input);
+    eval();
+    int n = sizeof(expects) / sizeof(struct Token);
+    for(int i = 0; i < n; i++) {
+        assert_token(stack_pop(stack), &expects[i]);
+    }
 }
 
 static void test_eval_num_one() {
@@ -85,6 +126,7 @@ int main() {
     test_eval_num_one();
     test_eval_num_two();
     test_eval_num_add();
+    test_eval_stack_literal_name();
 
     return 0;
 }
