d561caf6ce99ff62eda4e80fa93563bc3ecd89f0
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-15 21:34:49 +0900

    Implement print_hex

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -6,9 +6,35 @@
 */
 .globl _start
 _start:
+    ldr r0,=0x101f1000
     ldr r1,=0xdeadbeaf
-    b print_hex
 print_hex:
-    // TODO: implement here
+    mov r2, #8
+loop:
+    mov r3, r1
+    mov r4, r2
+skip:
+    and r5, r3, #0xF
+    lsr r3, r3, #4
+    sub r4, r4, #1
+    cmp r4, #0
+    bne skip
+    cmp r5, #10
+    bge print_alpha
+print_number:
+    add r5, r5, #0x30
+    b print
+print_alpha:
+    add r5, r5, #55
+print:
+    str r5, [r0]
+    sub r2, r2, #1
+    cmp r2, #0
+    bne loop
+    b end
+    str r5, [r0]
+    sub r2, r2, #1
+    cmp r2, #0
+    bne loop
 end:
     b end
