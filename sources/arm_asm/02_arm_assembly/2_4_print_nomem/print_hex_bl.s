45b8c009de535f7cccf4e61444a85971eaac501a
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-16 21:55:33 +0900

    Implement print_hex_bl

--- sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
+++ sources/arm_asm/02_arm_assembly/2_4_print_nomem/print_hex_bl.s
@@ -1,3 +1,9 @@
+/*
+ arm-none-eabi-as print_hex_bl.s -o print_hex_bl.o
+ arm-none-eabi-ld print_hex_bl.o -Ttext 0x00010000 -o print_hex_bl.elf
+ arm-none-eabi-objcopy print_hex_bl.elf -O binary print_hex_bl.bin
+ qemu-system-arm -M versatilepb -m 128M -nographic -kernel print_hex_bl.bin -serial mon:stdio
+*/
 .globl _start
 _start:
 
@@ -12,4 +18,25 @@ end:
 
 
 print_hex:
-  // TODO: write here!
+    ldr r1,=0x101f1000
+    mov r2, #32
+loop:
+    sub r2, r2, #4
+    lsr r3, r0, r2
+    and r3, r3, #0xF
+    cmp r3, #10
+    bge print_alpha
+print_number:
+    add r3, r3, #0x30
+    b print
+print_alpha:
+    add r3, r3, #55
+print:
+    str r3, [r1]
+    cmp r2, #0
+    bne loop
+    mov r3,#0x0D
+    str r3,[r1]
+    mov r3,#0x0A
+    str r3,[r1]
+    mov r15, r14
