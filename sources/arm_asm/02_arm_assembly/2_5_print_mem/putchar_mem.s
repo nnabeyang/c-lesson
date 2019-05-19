1d43f2f7e7e65780b31705e442716037a211ed4f
Noriaki Watanabe <nabeyang@gmail.com>
2019-05-19 10:14:42 +0900

    Implement print in putchar_mem.s

--- sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
+++ sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
@@ -1,6 +1,12 @@
+/*
+ arm-none-eabi-as putchar_mem.s -o putchar_mem.o
+ arm-none-eabi-ld putchar_mem.o -Ttext 0x00010000 -o putchar_mem.elf
+ arm-none-eabi-objcopy putchar_mem.elf -O binary putchar_mem.bin
+ qemu-system-arm -M versatilepb -m 128M -nographic -kernel putchar_mem.bin -serial mon:stdio
+*/
 .globl _start
 _start:
-
+  ldr r13,=0x08000000
   ldr r0,=msg1
   bl print
   ldr r0,=msg2
@@ -9,22 +15,26 @@ end:
   b end
 
 putchar:
-  // TODO: fix here!
+  stmdb r13!, {r1}
   ldr r1,=0x101f1000
-  ldr r0, [r1]
+  str r3, [r1]
+  ldmia r13!, {r1}
   mov r15, r14
 
 
 print:
-  // TODO: Fix this function too.
+  stmdb r13!, {r3, r14}
+  ldr r1,=0x101f1000
   ldrb r3,[r0]
 _loop:  
-  
 
+
+  bl putchar
   add r0, r0, #1
   ldrb r3,[r0]
   cmp r3,#0
   bne _loop
+  ldmia r13!, {r3, r14}
   mov r15, r14
 
 msg1: .asciz "First text.\n"
